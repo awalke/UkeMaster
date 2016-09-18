6947e0e77a27872cef40bed7f8831af2c07aa6a7
Allison Walke <awalke@ancestry.com>
2016-09-18 06:03:18 -0600

    first commit

---
+++ UkeMasterv2/SeeTheLightViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SeeTheLightViewController.swift
+//  UkeMasterv2
+//
+//  Created by Allison Walke on 9/18/16.
+//  Copyright © 2016 HacktheNorth. All rights reserved.
+//
+
+import UIKit
+
+class SeeTheLightViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
